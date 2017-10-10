9560c3dff517ee078868b7ef82d245622cfe120a
Ender50 <amjadsharafi10@gmail.com>
2017-10-10 12:58:09 +0300

    firstCommet

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GLCapabilities;
+import org.lwjgl.opengl.GLXARBCreateContextProfile;
+
+import static ExtraUtilities.OutStream.*;
+
+public class Main {
+    public static void main (String [] args){
+//        int context = GLXARBCreateContextProfile.GLX_CONTEXT_CORE_PROFILE_BIT_ARB;
+//        GLCapabilities glCapabilities = GL.createCapabilities();
+
+        GLFW.glfwInit();
+        long window = GLFW.glfwCreateWindow(400, 400, "this is a test", 0, 0);
+        GLFW.glfwMakeContextCurrent(window);
+        println(window);
+        GLFW.glfwShowWindow(window);
+        while(true){}
+    }
+
+}
